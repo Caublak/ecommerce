650bec39f2a24c23206c85ace92fdf4612fae182
Caublak <chetanaubeeluck0327@gmail.com>
2023-02-21 00:49:21 +0400

    CMS-login complete

--- cms/login.php
+++ cms/login.php
@@ -27,7 +27,7 @@
         <form onsubmit action="getadminLogin.php" method="post">
             <div id="conlogin">
                 <h2 id="Login-title">ADMIN LOGIN</h2>
-                <!-- <img src="../images/LogIcon.jpg" class="Login-icon"> -->
+                <img src="./images/LogIcon.jpg" class="Login-icon">
 
                 <!-- Entry box for username -->
                 <input class="loginput" id="uname" type="text" placeholder="Username" name="uname" required />
